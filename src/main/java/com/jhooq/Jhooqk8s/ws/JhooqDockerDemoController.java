a5eb58e7af487191d7d561ceecd58e309acb5a6d
ArunAVK <72615214+AVKArun@users.noreply.github.com>
2021-11-18 02:29:29 +0530

    Create JhooqDockerDemoController.java

---
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -0,0 +1,13 @@
+package com.jhooq.Jhooqk8s.ws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JhooqDockerDemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello - Arun AVK, this is your assignment 1 of devops.";
+    }
+}
